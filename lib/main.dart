bb5cbd0d6ba5161f5977c0dc4f8dc94b5d8eb5b7
nicopcat <tianfang07@outlook.com>
2023-01-17 23:47:13 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Center(
+      child: Text(
+    'Hellow World',
+    textDirection: TextDirection.ltr,
+    // textScaleFactor: 2.0,
+    style: TextStyle(
+      color: Colors.orange,
+      fontSize: 34,
+    ),
+  )));
+}
