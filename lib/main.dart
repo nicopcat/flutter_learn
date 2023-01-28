4e6c8c14a41136d64b5f2ecb3f35dc64fe6ecdca
nicopcat <tianfang07@outlook.com>
2023-01-29 00:06:59 +0800

    Text()组件

--- lib/main.dart
+++ lib/main.dart
@@ -12,105 +12,22 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text("HelloWorld"),
         ),
-        body: HomeBody(),
+        body: ContentWidget(),
       ),
     );
   }
 }
 
-class HomeBody extends StatelessWidget {
+
+class ContentWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    print("HomeBody build");
-    return MyCounterWidget();
-  }
-}
-
-class MyCounterWidget extends StatefulWidget {
-  MyCounterWidget() {
-    print("执行了MyCounterWidget的构造方法");
-  }
-
-  @override
-  State<StatefulWidget> createState() {
-    print("执行了MyCounterWidget的createState方法");
-    // 将创建的State返回
-    return MyCounterState();
-  }
-}
-
-class MyCounterState extends State<MyCounterWidget> {
-  int counter = 0;
-
-  MyCounterState() {
-    print("执行MyCounterState的构造方法");
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    print("执行MyCounterState的init方法");
-  }
-
-  @override
-  void didChangeDependencies() {
-    // TODO: implement didChangeDependencies
-    super.didChangeDependencies();
-    print("执行MyCounterState的didChangeDependencies方法");
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    print("执行执行MyCounterState的build方法");
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              ElevatedButton(
-                child: Text(
-                  "+1",
-                  style: TextStyle(fontSize: 18, color: Colors.white),
-                ),
-                onPressed: () {
-                  setState(() {
-                    counter++;
-                  });
-                },
-              ),
-              ElevatedButton(
-                child: Text(
-                  "-1",
-                  style: TextStyle(fontSize: 18, color: Colors.white),
-                ),
-                onPressed: () {
-                  setState(() {
-                    counter--;
-                  });
-                },
-              )
-            ],
-          ),
-          Text(
-            "当前计数：$counter",
-            style: TextStyle(fontSize: 30),
-          )
-        ],
-      ),
-    );
-  }
-
-  @override
-  void didUpdateWidget(MyCounterWidget oldWidget) {
-    super.didUpdateWidget(oldWidget);
-    print("执行MyCounterState的didUpdateWidget方法");
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    print("执行MyCounterState的dispose方法");
+    return Text.rich(
+        textAlign: TextAlign.center,
+        TextSpan(children: [
+          TextSpan(text: '《定风波》', style: TextStyle(color: Colors.deepPurpleAccent, fontSize: 30, fontWeight: FontWeight.bold)),
+          TextSpan(text: '苏轼', style: TextStyle(color: Colors.blueGrey, fontSize: 22)),
+          TextSpan(text: '\n莫听穿林打叶声，何妨吟啸且徐行。\n竹杖芒鞋轻胜马，谁怕？一蓑烟雨任平生。', style: TextStyle(color: Colors.black26, fontSize: 20)),
+        ]));
   }
 }
