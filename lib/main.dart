b271b8d4430c2ad9d1a33c87253972bfcf01e4da
Nic <tianfang07@outlook.com>
2023-01-29 16:00:34 +0800

    button组件

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,48 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const Center(
-      child: Text(
-    'Hellow World',
-    textDirection: TextDirection.ltr,
-    // textScaleFactor: 2.0,
-    style: TextStyle(
-      color: Colors.orange,
-      fontSize: 34,
-    ),
-  )));
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'hellow',
+        home: Scaffold(
+          appBar: AppBar(title: Text('标题')),
+          body: ButtonWidgets(),
+        ));
+  }
+}
+
+class ButtonWidgets extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ElevatedButton(
+            onPressed: () {
+              print('按了一下ElevatedButton');
+            },
+            style: const ButtonStyle(backgroundColor: MaterialStatePropertyAll<Color>(Colors.redAccent)),
+            child: const Text('ElevatedButton')),
+        TextButton(
+            onPressed: () {
+              print('TextButton');
+            },
+            style: const ButtonStyle(backgroundColor: MaterialStatePropertyAll<Color>(Colors.redAccent)),
+            child: const Text('TextButton')),
+        OutlinedButton(
+            onPressed: () {
+              print('OutlineButton');
+            },
+            child: const Text('OutlineButton')),
+        IconButton(
+            onPressed: () {
+              print('IconButton');
+            },
+            icon: const Icon(Icons.add)),
+        ElevatedButton.icon(onPressed: () {}, icon: const Icon(Icons.send), label: const Text('发生'))
+      ],
+    );
+  }
 }
