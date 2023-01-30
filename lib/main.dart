dd9c0ab825f114f866ab393dbb96c1e328f398b6
Nic <tianfang07@outlook.com>
2023-01-30 16:30:52 +0800

    from表单组件学习

--- lib/main.dart
+++ lib/main.dart
@@ -19,32 +19,91 @@ class ContentWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView(
       children: [
-        FormDemo()
+        Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: LoginWidget(),
+        )
       ],
     );
   }
 }
 
-class FormDemo extends StatefulWidget {
+class LoginWidget extends StatefulWidget {
   @override
-  _FormDemoState createState() => _FormDemoState();
+  _LoginWidgetState createState() => _LoginWidgetState();
 }
 
-class _FormDemoState extends State {
+class _LoginWidgetState extends State {
+  final registerFormKey = GlobalKey<FormState>();
+
+  String phoneNumber = '';
+  String username = '';
+  String password = '';
+
+  void logginAction() {
+    registerFormKey.currentState?.save();
+    // registerFormKey.currentState?.validate();
+
+    Map<String, dynamic> parameters = {};
+    parameters['username'] = username;
+    parameters['password'] = password;
+    parameters['phoneNumber'] = phoneNumber;
+    print(parameters);
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Form(
+        key: registerFormKey,
         child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        TextFormField(
-          decoration: InputDecoration(icon: Icon(Icons.people), labelText: '用户名'),
-        ),
-        TextFormField(
-          decoration: InputDecoration(icon: Icon(Icons.phone), labelText: '手机号'),
-        )
-      ],
-    ));
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextFormField(
+                decoration: InputDecoration(icon: Icon(Icons.people), labelText: '用户名'),
+                onSaved: (e) {
+                  e ??= '';
+                  username = e;
+                },
+                validator: (v) {
+                  if (v == null || v.length == 0) {
+                    return '请输入用户名';
+                  }
+                  return null;
+                },
+                autovalidateMode: AutovalidateMode.onUserInteraction
+                // onChanged: (v) {
+                //   setState(() {
+                //     username = v;
+                //   });
+                // },
+                ),
+            TextFormField(
+                decoration: InputDecoration(icon: Icon(Icons.phone), labelText: '手机号'),
+                onSaved: (e) {
+                  e ??= '';
+                  phoneNumber = e;
+                }),
+            TextFormField(
+              decoration: InputDecoration(icon: Icon(Icons.lock), labelText: '密码'),
+              obscureText: true,
+              onChanged: (v) {
+                setState(() {
+                  password = v;
+                });
+              },
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            ElevatedButton.icon(
+              label: Text('提交'),
+              onPressed: () {
+                logginAction();
+              },
+              icon: Icon(Icons.send),
+            )
+          ],
+        ));
   }
 }
